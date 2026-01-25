93fa8b73b600bcb9697843ebfe7d5025131fd658
Evgheni <evgheni.nemerenco@gmail.com>
2026-01-26 00:43:21 +0200

    Refactored pricing rule application

--- advanced_pricing_rules/__init__.py
+++ advanced_pricing_rules/__init__.py
@@ -1,9 +1,7 @@
 __version__ = "0.0.1"
 
-import frappe
-import erpnext.accounts.doctype.pricing_rule.pricing_rule as pricing_rule_module
 import erpnext.accounts.doctype.pricing_rule.utils as pricing_rule_utils
-import erpnext.stock.get_item_details as get_item_details
-from advanced_pricing_rules.custom_pricing_rule import apply_price_discount_rule_patch
+from advanced_pricing_rules.custom_pricing_rule import _get_pricing_rules_patch
 
-pricing_rule_module.apply_price_discount_rule = apply_price_discount_rule_patch
+#pricing_rule_module.apply_price_discount_rule = apply_price_discount_rule_patch
+pricing_rule_utils._get_pricing_rules = _get_pricing_rules_patch
--- advanced_pricing_rules/custom_pricing_rule.py
+++ advanced_pricing_rules/custom_pricing_rule.py
@@ -1,18 +1,21 @@
-import copy
-import json
 import frappe
-from erpnext.accounts.doctype.pricing_rule.pricing_rule import apply_price_discount_rule
-from erpnext.accounts.doctype.pricing_rule.utils import get_pricing_rules
+from erpnext.accounts.doctype.pricing_rule.utils import _get_pricing_rules
 from erpnext.stock.get_item_details import get_price_list_rate_for
 
-@frappe.whitelist()
-def apply_price_discount_rule_patch(pricing_rule, item_details, args=None):
-    if 'advanced_pricing_rules' in frappe.get_installed_apps():
-        cpr = copy.copy(pricing_rule);
-        if cpr.get("rate_or_discount") == "Rate from Price List" and cpr.get("custom_rate_from_price_list"):
+def _get_pricing_rules_patch(apply_on, args, values):
+    pricing_rules = _get_pricing_rules(apply_on, args, values)
+
+    for rule in pricing_rules:
+        if rule.get('rate_or_discount') == 'Rate from Price List' and rule.get("custom_rate_from_price_list"):
+            standard_rate = (
+                args.get("price_list_rate")
+                or args.get("base_price_list_rate")
+                or 0
+            )
+
             ctx = frappe._dict(
                 {
-                    "price_list": cpr.get("custom_rate_from_price_list"),
+                    "price_list": rule.get("custom_rate_from_price_list"),
                     "uom": args.get("uom"),
                     "transaction_date": args.get("transaction_date"),
                     "customer": args.get("customer"),
@@ -21,7 +24,13 @@ def apply_price_discount_rule_patch(pricing_rule, item_details, args=None):
                 }
             )
 
-            cpr.rate_or_discount = "Rate"
-            cpr.rate = get_price_list_rate_for(ctx, args.get("item_code"))
+            alt_rate = get_price_list_rate_for(ctx, args.get("item_code")) or 0
 
-    return apply_price_discount_rule(cpr, item_details, args)
+            # Apply discount amount as positive difference when alt_rate is lower.
+            discount_amount = max(0, float(standard_rate) - float(alt_rate))
+
+            # Switch rule type to Discount Amount so ERPNext applies it correctly.
+            rule["rate_or_discount"] = "Discount Amount"
+            rule["discount_amount"] = discount_amount
+
+    return pricing_rules
